5d510763fe4883d9efdfd4393be96f37925068a2
kewablay <kojokewajunior123@gmail.com>
2024-11-19 21:22:35 +0000

    feat: add initial cta-section

---
+++ src/app/components/cta-section/cta-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cta-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './cta-section.component.html',
+  styleUrl: './cta-section.component.sass'
+})
+export class CtaSectionComponent {
+
+}
