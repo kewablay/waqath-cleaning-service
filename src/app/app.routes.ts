eab3be6d1810c90a9879542adfc84f320ff4e0dc
kewablay <kojokewajunior123@gmail.com>
2024-11-19 21:26:42 +0000

    feat: add initial app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'book-us',
+    loadComponent: () =>
+      import('./pages/book-us/book-us.component').then(
+        (m) => m.BookUsComponent
+      ),
+  },
+];
