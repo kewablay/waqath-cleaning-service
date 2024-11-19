f9f8c37049df382f3077161bb793b34fa116adc3
kewablay <kojokewajunior123@gmail.com>
2024-11-19 21:21:28 +0000

    feat: adde initial book-us page

---
+++ src/app/pages/book-us/book-us.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-us',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-us.component.html',
+  styleUrl: './book-us.component.sass'
+})
+export class BookUsComponent {
+
+}
