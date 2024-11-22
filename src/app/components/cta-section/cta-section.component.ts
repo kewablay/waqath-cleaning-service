cceadb10c14cbb72615993a5d9a120e421273382
kewablay <kojokewajunior123@gmail.com>
2024-11-22 13:07:38 +0000

    feat: add finial cta section, with responsiveness

--- src/app/components/cta-section/cta-section.component.ts
+++ src/app/components/cta-section/cta-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cta-section',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cta-section.component.html',
   styleUrl: './cta-section.component.sass'
 })
