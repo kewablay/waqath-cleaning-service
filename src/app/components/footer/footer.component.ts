0a7b4e9210213e9ca67e9cbba7436689faab27ed
kewablay <kojokewajunior123@gmail.com>
2024-11-22 13:08:54 +0000

    feat: add footer with responsiveness

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,32 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.sass'
+  styleUrl: './footer.component.sass',
 })
 export class FooterComponent {
+  contactInfo = {
+    phone: '+33 5 63 44 67 77',
+    address: '123 Tech Street, Suite 101',
+    city: 'Cityville, State, ZIP',
+    email: 'Contact@company.com',
+  };
 
+  navigation = {
+    about: [
+      { title: 'About us', link: '/about' },
+      { title: 'Blog', link: '/blog' },
+      { title: 'Contact', link: '/contact' },
+    ],
+    social: [
+      { title: 'Instagram', link: 'https://instagram.com' },
+      { title: 'Facebook', link: 'https://facebook.com' },
+      { title: 'X', link: 'https://x.com' },
+    ],
+  };
+
+  contactLinks = [
+    { icon: 'whatsapp', link: 'https://wa.me/33563446777' },
+    { icon: 'telegram', link: 'https://t.me/yourcompany' },
+    { icon: 'gmail', link: 'mailto:contact@company.com' },
+  ];
 }
