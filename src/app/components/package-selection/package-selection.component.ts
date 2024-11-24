d5c05ba088cb58c62a49aaeafa86c1fa8cd262a0
kewablay <kojokewajunior123@gmail.com>
2024-11-24 16:15:45 +0000

    feat: add initial package selection component

---
+++ src/app/components/package-selection/package-selection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-package-selection',
+  standalone: true,
+  imports: [],
+  templateUrl: './package-selection.component.html',
+  styleUrl: './package-selection.component.sass'
+})
+export class PackageSelectionComponent {
+
+}
