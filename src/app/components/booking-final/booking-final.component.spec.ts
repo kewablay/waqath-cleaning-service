dd815abbd677853bf8a38fd393200336adfae0b8
kewablay <kojokewajunior123@gmail.com>
2024-11-24 16:13:36 +0000

    feat: add booking-final component

---
+++ src/app/components/booking-final/booking-final.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookingFinalComponent } from './booking-final.component';
+
+describe('BookingFinalComponent', () => {
+  let component: BookingFinalComponent;
+  let fixture: ComponentFixture<BookingFinalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BookingFinalComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(BookingFinalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/booking-final/booking-final.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-final',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-final.component.html',
+  styleUrl: './booking-final.component.sass'
+})
+export class BookingFinalComponent {
+
+}
