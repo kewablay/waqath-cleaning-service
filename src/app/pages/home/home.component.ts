88b3ed2a8a48f97adf09e8b1e0119a9150fd1833
kewablay <kojokewajunior123@gmail.com>
2024-11-24 16:14:54 +0000

    feat: add all sections to home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FAQComponent } from '../../components/faq/faq.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
+import { CtaSectionComponent } from '../../components/cta-section/cta-section.component';
+import { ServicesComponent } from '../../components/services/services.component';
+import { WhyChooseUsComponent } from '../../components/why-choose-us/why-choose-us.component';
+import { HeroSectionComponent } from '../../components/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    TestimonialsComponent,
+    CtaSectionComponent,
+    ServicesComponent,
+    WhyChooseUsComponent,
+    HeroSectionComponent,
+    FAQComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.sass',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
