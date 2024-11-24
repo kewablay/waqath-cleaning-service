aacdee1783c40e2cf862bc8837631eaada2cdd43
kewablay <kojokewajunior123@gmail.com>
2024-11-24 16:16:26 +0000

    feat: add interfaces for app

---
+++ src/app/models/app.model.ts
@@ -0,0 +1,34 @@
+export interface BookingState {
+  step: number;
+  maxSteps: number;
+  formValid: boolean;
+}
+
+export interface PersonalInfo {
+  fullName: string;
+  email: string;
+  phone: string;
+  address: string;
+}
+export interface CleaningPreferences {
+  serviceType: string;
+  frequency: string;
+  preferredDays: string[];
+  professionals: number;
+}
+export interface PackageDetails {
+  provideTools: boolean;
+  pricePerHour: number;
+}
+
+export interface BookingFinal {
+  specialInstructions: string;
+  paymentMethod: string;
+}
+
+export type BookingForm = {
+    personalInfo: PersonalInfo;
+    preferences: CleaningPreferences;
+    package: PackageDetails;
+    final: BookingFinal;
+};
--- src/app/pages/book-us/book-us.component.ts
+++ src/app/pages/book-us/book-us.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BookUsHeaderComponent } from "../../components/book-us-header/book-us-header.component";
+import { BookUsContentComponent } from "../../components/book-us-content/book-us-content.component";
 
 @Component({
   selector: 'app-book-us',
   standalone: true,
-  imports: [],
+  imports: [ BookUsHeaderComponent, BookUsContentComponent],
   templateUrl: './book-us.component.html',
   styleUrl: './book-us.component.sass'
 })
