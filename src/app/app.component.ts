c056d7945dbe4c30c6192e27fdb2e3443cac1f02
kewablay <kojokewajunior123@gmail.com>
2024-11-25 09:05:11 -0800

    feat: inject toast service in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ToastModule } from 'primeng/toast';
+import { ToastService } from './services/toast-service/toast.service';
+import { CustomToastComponent } from "./components/custom-toast/custom-toast.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ToastModule, CustomToastComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'waqath-cleaning-service';
+  constructor(public toastService: ToastService) {}
 }
