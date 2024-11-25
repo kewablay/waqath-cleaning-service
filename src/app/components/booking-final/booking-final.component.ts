227f801400a609244cfaf947c8cd67a6c09c4823
kewablay <kojokewajunior123@gmail.com>
2024-11-25 05:55:15 -0800

    feat: implement booking final form and service integration

--- src/app/components/booking-final/booking-final.component.ts
+++ src/app/components/booking-final/booking-final.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { BookingService } from '../../services/booking.service';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { PaymentMethod } from '../../models/app.model';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-booking-final',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule,
+    InputTextareaModule,
+    DropdownModule,
+  ],
   templateUrl: './booking-final.component.html',
-  styleUrl: './booking-final.component.sass'
+  styleUrl: './booking-final.component.sass',
 })
 export class BookingFinalComponent {
+  bookingFinalForm: FormGroup;
+  paymentMethods: PaymentMethod[] = [
+    { label: 'Credit Card', value: 'credit-card' },
+    { label: 'PayPal', value: 'paypal' },
+    { label: 'Bank Transfer', value: 'bank-transfer' },
+  ];
 
+  constructor(private fb: FormBuilder, private bookingService: BookingService) {
+    this.bookingFinalForm = this.fb.group({
+      specialInstructions: [''],
+      paymentMethod: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.bookingFinalForm.valid) {
+      this.bookingService.updateFormData({
+        final: this.bookingFinalForm.value,
+      });
+      this.bookingService.nextStep();
+    }
+  }
 }
