8b7a06af052a177bb9f3a91c730e1c8ad728d901
kewablay <kojokewajunior123@gmail.com>
2024-11-26 02:06:35 -0800

    feat: add navigation method

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, SidebarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass'
 })
 export class NavbarComponent {
+  sidebarVisible: boolean = false;
+  constructor(private viewportScroller: ViewportScroller) {}
 
+  navigateToSection(elementId: string): void {
+    this.sidebarVisible = false;
+    
+    setTimeout(() => {
+      this.viewportScroller.scrollToAnchor(elementId);
+    }, 200);
+  }
 }
